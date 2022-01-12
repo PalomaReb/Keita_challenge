8e14457000892493d9b72ea278ca352a935a5e6e
PalomaReb <paloma.rebolleda@gmail.com>
2022-01-12 22:14:26 +0100

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import React from 'react';
+import React from "react";
 //import Login from './pages/login';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "./theme/theme";
 import { Container } from "@material-ui/core";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
-import SubForm from './components/subForm';
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SubForm from "./components/subForm";
+import Login from "./pages/login";
+import SubsList from "./pages/subsList";
 
 function App() {
-
   return (
-        <ThemeProvider theme={theme}>
-          <Container>
-          <SubForm></SubForm>
-        {/* <Router>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Router>
           <Routes>
-            <Route element={<Login/>} path="/">
-              </Route>
-              </Routes>
-            </Router> */}
-            </Container>
-      </ThemeProvider>
+            <Route element={<Login />} path="/"></Route>
+          </Routes>
+          <Routes>
+            <Route element={<SubForm />} path="/create-subsidies"></Route>
+          </Routes>
+          <Routes>
+            <Route element={<SubsList />} path="/view-subsidies"></Route>
+          </Routes>
+        </Router>
+      </Container>
+    </ThemeProvider>
   );
 }
-
 
 export default App;
