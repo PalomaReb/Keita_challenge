7903d0f260b94abde6a75795ca798f7ae8cc8f64
PalomaReb <paloma.rebolleda@gmail.com>
2022-01-13 22:03:35 +0100

    header update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,77 @@
-import React from "react";
-import { Button, Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, Grid, Menu, MenuItem } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import { useStyles } from "../pages/mainCSS";
 import logo from "../assets/keitaLogo.jpg";
+import UserAuth from "../classes/user";
+import { useNavigate } from "react-router-dom";
 
+export function Header() {
+  const classes = useStyles();
+  const navigate = useNavigate();
 
-export function Header(){
-    const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    return(
-
-        <Grid container direction="row" justifyContent="space-between" className={classes.header}>
-            <Grid item>
-                <img alt="logo" className={classes.logo} src={logo} />
-            </Grid>
-            <Grid item>
-                <Button> Create new subsidery</Button>
-                <Button>View all subsideries</Button>
-            </Grid>
-            <Grid item>
-                <Button>Log out </Button>
-            </Grid>
-        </Grid>
-    
-   
-    );
-
-
-
-
-
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      className={classes.header}
+    >
+      <Grid item>
+        <img alt="logo" className={classes.logo} src={logo} />
+      </Grid>
+      <Grid className={classes.desktopMenu} item>
+        <Button
+          onClick={() => navigate("/create-subsidies")}
+          className={classes.button}
+        >
+          Create new subsidy
+        </Button>
+        <Button
+          onClick={() => navigate("/view-subsidies")}
+          className={classes.button}
+        >
+          View subsidies
+        </Button>
+      </Grid>
+      <Grid className={classes.desktopMenu} item>
+        <Button
+          onClick={() => UserAuth.logout(() => navigate("/"))}
+          className={classes.button}
+        >
+          Logout
+        </Button>
+      </Grid>
+      <Button
+        className={classes.mobileMenu}
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MenuIcon></MenuIcon>
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+      >
+        <MenuItem onClick={() => navigate("/view-subsidies")}>
+          View subsidies
+        </MenuItem>
+        <MenuItem onClick={() => navigate("/create-subsidies")}>
+          Create new subsidy
+        </MenuItem>
+        <MenuItem onClick={() => UserAuth.logout(() => navigate("/"))}>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Grid>
+  );
 }
-
