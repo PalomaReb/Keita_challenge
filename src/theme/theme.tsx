7a62225490b3e1f47643920fc94444065adaae05
PalomaReb <paloma.rebolleda@gmail.com>
2022-01-13 22:03:50 +0100

    change in theme colors

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,55 +1,53 @@
 import { createTheme } from "@material-ui/core";
 
-
 export const theme = createTheme({
-    palette: {
-        text: {
-            primary: "#000000",
-            secondary: "#1da3cf",
-            hint:"#000000",
-        },
-        primary: {
-            main: "#1da3cf",
-        },
-        secondary: {
-            main: "#ffffff",
-        },
-        warning: {
-            main: "#fe1807",
-        },
-        background: {
-            default: "#1E3640",
-        }
-
+  palette: {
+    text: {
+      primary: "#000000",
+      secondary: "#1da3cf",
+      hint: "#00bc22",
     },
-    typography: {
-        h1: {
-            fontSize: 30,
-            fontWeight:"bold",
-            marginBottom: ".6rem",
-        },
-        h2: {
-            fontSize: 24,
-            fontWeight:"bold",
-        },
-        h3: {
-            fontSize: 19,
-            fontWeight: "bold"
-        },
-        h4: {
-            fontSize: 16,
-        },
+    primary: {
+      main: "#1da3cf",
     },
-    shape: {
-        borderRadius: 20,
+    secondary: {
+      main: "#ffffff",
     },
-    breakpoints: {
-        values: {
-            xs: 0,
-            sm: 600,
-            md: 960,
-            lg: 1280,
-            xl: 1920,
-        },
+    warning: {
+      main: "#fe1807",
     },
+    background: {
+      default: "#1E3640",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: 30,
+      fontWeight: "bold",
+      marginBottom: ".6rem",
+    },
+    h2: {
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    h3: {
+      fontSize: 19,
+      fontWeight: "bold",
+    },
+    h4: {
+      fontSize: 16,
+    },
+  },
+  shape: {
+    borderRadius: 20,
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
 });
